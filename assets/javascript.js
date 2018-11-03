3f769a709dc275cc4b34aa12c37854f6a142e09f
Jamie <alaryj@gmail.com>
2018-11-03 09:50:52 -0600

    latest updates

--- assets/javascript.js
+++ assets/javascript.js
@@ -7,11 +7,4 @@ $("div[id^='myModal']").each(function(){
       currentModal.modal('hide');
       currentModal.closest("div[id^='myModal']").nextAll("div[id^='myModal']").first().modal('show'); 
     });
-    
-    //click prev
-    currentModal.find('.btn-prev').click(function(){
-      currentModal.modal('hide');
-      currentModal.closest("div[id^='myModal']").prevAll("div[id^='myModal']").first().modal('show'); 
-    });
-  
-  });
+});
